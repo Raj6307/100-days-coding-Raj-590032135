ab66fad5d21f1fc554f30f0e680d135867ef3ef4
Raj6307 <okayraj63@gmail.com>
2026-08-25 23:58:43 +0530

    Add files via upload

---
+++ day16b.c
@@ -0,0 +1,25 @@
+//Write a program to check if a number is a palindrome.
+#include <stdio.h>
+
+int main() {
+	int number, original, reversed = 0, remainder;
+
+	printf("Enter a number: ");
+	scanf("%d", &number);
+
+	original = number;
+
+	while (number != 0) {
+		remainder = number % 10;
+		reversed = reversed * 10 + remainder;
+		number /= 10;
+	}
+
+	if (original == reversed) {
+		printf("Palindrome\n");
+	} else {
+		printf("Not palindrome\n");
+	}
+
+	return 0;
+}
